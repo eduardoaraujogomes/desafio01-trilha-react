2e16af85e6ae402d95b6d1889b69c6758ea6b2f1
Eduardo de Araújo <eduardoaraujogf@gmail.com>
2021-03-13 12:36:21 -0300

    feature: created the funcionality of remove itens

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -34,6 +34,8 @@ export function TaskList() {
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const filteredTasks = tasks.filter(task => task.id !== id);
+    setTasks(filteredTasks);
   }
 
   return (
